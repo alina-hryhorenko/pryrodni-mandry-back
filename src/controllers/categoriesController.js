fb8317bf6e948ab8f8fdac416f33fef4bc22913e
damirmamian <damirmamian@gmail.com>
2026-07-06 19:24:30 +0300

    categories controller

---
+++ src/controllers/categoriesController.js
@@ -0,0 +1,25 @@
+import { Category } from '../models/category.js';
+import { Story } from '../models/story.js';
+
+export const getCategoriesWithCount = async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+
+    const result = await Promise.all(
+      categories.map(async (item) => {
+        const count = await Story.countDocuments({
+          category: item.category,
+        });
+
+        return {
+          category: item.category,
+          count,
+        };
+      }),
+    );
+
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
