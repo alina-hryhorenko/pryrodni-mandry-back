47c27b72aed351a5ff2f14b3bfe7f1192f42a9a0
arstrong <arsendacenko89@gmail.com>
2026-07-06 23:48:10 +0300

    add/getStoryByStoryId

--- src/controllers/storiesController.js
+++ src/controllers/storiesController.js
@@ -57,9 +57,27 @@ export const getPopularStories = (req, res) => {
     res.res(200).json();
 };
 
-export const getStoryByStoryId = (req, res) => {
+export const getStoryByStoryId = async(req, res) => {
+  try {
+    const { storyId } = req.params;
 
-    res.res(200).json();
+    
+    const story = await Story.findById(storyId)
+      .populate('ownerId');
+
+    if (!story) {
+      return res.status(404).json({
+        message: 'Така історія відсутня',
+      });
+    }
+
+    return res.status(200).json(story);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Помилка сервера',
+      error: error.message,
+    });
+  }
 };
 
 export const getStoryByUserId = (req, res) => {
