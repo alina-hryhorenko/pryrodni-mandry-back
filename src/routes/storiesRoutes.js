39d20fc6be003c839e9417307b152d1cb9f68a3e
arstrong <arsendacenko89@gmail.com>
2026-07-06 14:04:17 +0300

    fix /stories contoller

--- src/routes/storiesRoutes.js
+++ src/routes/storiesRoutes.js
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import { 
     getAllStories, 
     getPopularStories, 
-    getStoriesByCategory, 
     getStoryByStoryId,
     getStoryByUserId,
     createStory
@@ -13,7 +12,6 @@ const router = Router();
 // GET
 router.get('/stories', getAllStories);
 router.get('/stories/popular', getPopularStories);
-router.get('/stories/:category', getStoriesByCategory);
 router.get('/story/:storyId', getStoryByStoryId);
 router.get('/stories/:userId', getStoryByUserId);
 
