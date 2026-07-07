529c5446e96e0297534ad5b796b5ce7ca7046fc4
Artem <stuzukartem12@gmail.com>
2026-07-07 13:17:38 +0300

    test

---
+++ src/validations/authValidation.js
@@ -0,0 +1,10 @@
+
+
+import { Joi, Segments } from 'celebrate';
+
+export const registerUserSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  }),
+};
