5e6fa9f19d1c94b20ab82962ac970f1cc0c029bc
arstrong <arsendacenko89@gmail.com>
2026-07-09 12:10:06 +0300

    Fixed avatar controller and image stories

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
+import 'dotenv/config';
 
 cloudinary.config({
   secure: true,
@@ -7,10 +8,40 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export async function saveFileToCloudinary(buffer, userId) {
+export async function saveAvatarFileToCloudinary(buffer, userId) {
   const option = {
     folder: 'nature-travels-app/avatars',
+    auto_mkdir: true,
     public_id: `avatar_${userId}`,
+    resource_type: 'image',
+    overwrite: true,
+    unique_filename: false,
+    transformation: [
+      { width: 500, height: 500, crop: 'fill', gravity: 'auto' },
+      { fetch_format: 'auto', quality: 'auto' },
+    ],
+  };
+
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      option,
+      (error, result) => {
+        if (error) {
+          return reject(error);
+        }
+        resolve(result);
+      },
+    );
+
+    uploadStream.end(buffer);
+  });
+}
+
+export async function saveStoryImageFileToCloudinary(buffer, storyImageId) {
+  const option = {
+    folder: 'nature-travels-app/story-images',
+    auto_mkdir: true,
+    public_id: `story_${storyImageId}`,
     resource_type: 'image',
     overwrite: true,
     unique_filename: false,
