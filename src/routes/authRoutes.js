81290619091222637270959231e2301ae027cd87
arstrong <arsendacenko89@gmail.com>
2026-07-10 17:38:17 +0300

    logout fixed

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,6 +8,7 @@ import {
 } from '../controllers/authController.js';
 import { registerUserSchema } from '../validations/authValidation.js';
 import { registerUser } from '../controllers/authController.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
@@ -17,7 +18,7 @@ router.post(
   loginUser,
 );
 
-router.post('/auth/logout', logoutUser);
+router.post('/auth/logout', authenticate, logoutUser);
 router.post('/auth/refresh', refreshUserSession);
 
 export default router;
